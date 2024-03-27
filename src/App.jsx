c3d88c82943fc2a195d293de60b0e9740c92b263
Brandie Lucano <blucano.dev@gmail.com>
2024-03-27 14:43:04 -0700

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,43 @@
+import { useState } from 'react';
+import logo from './logo.svg'
+import './App.css';
+import About from './About';
+
+
+
+/** Component for entire page.
+ *
+ * Props: none
+ * State: none
+ *
+*/
+
+function App() {
+  // const [count, setCount] = useState(0);
+
+  // function incrCount() {
+  //   setCount(count => count + 1);
+  // }
+
+  return (
+    <div className="App">
+      {/* <main>
+        <img src={logo} className="App-logo" alt="Rithm" />
+        <h1>Rithm React Starter</h1>
+        <p>
+          <button
+              className="btn btn-primary"
+              onClick={incrCount}>
+            Clicked: {count}
+          </button>
+        </p>
+        <p>
+          Edit <code>src/App.jsx</code>
+        </p>
+      </main> */}
+      <About />
+    </div>
+  );
+};
+
+export default App;
