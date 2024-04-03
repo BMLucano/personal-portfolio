f9dc60109edac29ee85adea1d1902e651ee993a6
Brandie Lucano <blucano.dev@gmail.com>
2024-04-03 15:39:23 -0700

    portrait mobile size fixed * need to fix overall responsiveness of portrait

--- src/About.jsx
+++ src/About.jsx
@@ -8,10 +8,10 @@ function About() {
         <div className="container-fluid">
             <div className="row mt-3">
                 <div className="col-md-4 order-md-2 d-flex align-items-center justify-content-center mb-2">
-                    <img className="portrait img-fluid" src={portrait} alt="Brandie Lucano-portrait" />
+                    <img className="portrait" src={portrait} alt="Brandie Lucano-portrait" />
                 </div>
                 <div className="col-md-8 order-md-1 text-light">
-                    <p>Hello! I'm a full-stack software engineer with a background in education. My journey into software engineering was fueled by a genuine passion for leveraging technology to enhance educational experiences. I've witnessed firsthand the transformative power that software can have in the classroom, and it's this intersection of education and technology that drives my passion for engineering.</p>
+                    <p>Hello! I'm a full-stack software engineer with a background in education. My journey into software engineering was fueled by a passion for leveraging technology to enhance educational experiences. I've witnessed firsthand the transformative power that software can have in the classroom, and it's this intersection of education and technology that drives my passion for engineering.</p>
                     <p>Beyond coding, I'm an advocate for accessibility in digital spaces, firmly believing that everyone should have equal access to the web. As an aspiring contributor to the growing field of educational technology, I'm excited about the prospect of merging my passions for software engineering and education to create meaningful impacts in the space of learning and beyond!</p>
                 </div>
             </div>
