1da9e7e4258de0aa99d2e72da85b1f9e7dd8f650
Brandie Lucano <blucano.dev@gmail.com>
2024-04-03 14:50:33 -0700

    svg files did not transfer with build *changed to import svg files

--- src/Projects.jsx
+++ src/Projects.jsx
@@ -1,5 +1,23 @@
 import ProjectCard from "./ProjectCard";
 import "./Projects.css"
+import Bootstrap from "./images/icons/Bootstrap.svg"
+import CSS from "./images/icons/CSS3.svg"
+import Django from "./images/icons/Django.svg"
+import Express from "./images/icons/Express.svg"
+import Flask from "./images/icons/Flask.svg"
+import Git from "./images/icons/Git.svg"
+import GitHub from "./images/icons/GitHub.svg"
+import HTML from "./images/icons/HTML5.svg"
+import Insomnia from "./images/icons/Insomnia.svg"
+import JavaScript from "./images/icons/JavaScript.svg"
+import Node from "./images/icons/Node.js.svg"
+import NPM from "./images/icons/NPM.svg"
+import PostgresSQL from "./images/icons/PostgresSQL.svg"
+import Python from "./images/icons/Python.svg"
+import React from "./images/icons/React.svg"
+import SQLAlchemy from "./images/icons/SQLAlchemy.svg"
+import TypeScript from "./images/icons/TypeScript.svg"
+import VSCode from "./images/icons/VSCode.svg"
 
 /** Project page for site. */
 let projectsInfo = [
@@ -36,24 +54,24 @@ function Projects() {
             <div className="toolbox-section bg-secondary border-primary p-3 mb-3">
                 <h1 className="text-left text-primary">My Toolbox:</h1>
                 <div className="d-flex flex-wrap align-items-center justifu-content-center">
-                    <img className="icon" src="src/images/icons/Bootstrap.svg" alt="Bootstrap icon"></img>
-                    <img className="icon" src="src/images/icons/CSS3.svg" alt="CSS icon"></img>
-                    <img className="icon" src="src/images/icons/Django.svg" alt="Django icon"></img>
-                    <img className="icon" src="src/images/icons/Express.svg" alt="Express icon"></img>
-                    <img className="icon" src="src/images/icons/Flask.svg" alt="Flask icon"></img>
-                    <img className="icon" src="src/images/icons/Git.svg" alt="Git icon"></img>
-                    <img className="icon" src="src/images/icons/GitHub.svg" alt="GitHub icon"></img>
-                    <img className="icon" src="src/images/icons/HTML5.svg" alt="HTML icon"></img>
-                    <img className="icon" src="src/images/icons/Insomnia.svg" alt="Insomnia icon"></img>
-                    <img className="icon" src="src/images/icons/JavaScript.svg" alt="JavaScript icon"></img>
-                    <img className="icon" src="src/images/icons/Node.js.svg" alt="Node.js icon"></img>
-                    <img className="icon" src="src/images/icons/NPM.svg" alt="NPM icon"></img>
-                    <img className="icon" src="src/images/icons/PostgresSQL.svg" alt="PostgresSQL icon"></img>
-                    <img className="icon" src="src/images/icons/Python.svg" alt="Python icon"></img>
-                    <img className="icon" src="src/images/icons/React.svg" alt="React icon"></img>
-                    <img className="icon" src="src/images/icons/SQLAlchemy.svg" alt="SQLAlchemy icon"></img>
-                    <img className="icon" src="src/images/icons/TypeScript.svg" alt="TypeScript icon"></img>
-                    <img className="icon" src="src/images/icons/VSCode.svg" alt="VSCode icon"></img>
+                    <img className="icon" src={Bootstrap} alt="Bootstrap icon"></img>
+                    <img className="icon" src={CSS} alt="CSS icon"></img>
+                    <img className="icon" src={Django} alt="Django icon"></img>
+                    <img className="icon" src={Express} alt="Express icon"></img>
+                    <img className="icon" src={Flask} alt="Flask icon"></img>
+                    <img className="icon" src={Git} alt="Git icon"></img>
+                    <img className="icon" src={GitHub} alt="GitHub icon"></img>
+                    <img className="icon" src={HTML} alt="HTML icon"></img>
+                    <img className="icon" src={Insomnia} alt="Insomnia icon"></img>
+                    <img className="icon" src={JavaScript} alt="JavaScript icon"></img>
+                    <img className="icon" src={Node} alt="Node.js icon"></img>
+                    <img className="icon" src={NPM} alt="NPM icon"></img>
+                    <img className="icon" src={PostgresSQL} alt="PostgresSQL icon"></img>
+                    <img className="icon" src={Python} alt="Python icon"></img>
+                    <img className="icon" src={React} alt="React icon"></img>
+                    <img className="icon" src={SQLAlchemy} alt="SQLAlchemy icon"></img>
+                    <img className="icon" src={TypeScript} alt="TypeScript icon"></img>
+                    <img className="icon" src={VSCode} alt="VSCode icon"></img>
                 </div>
             </div>
             <h1 className="text-success mt-2 mb-3">Featured Work</h1>
