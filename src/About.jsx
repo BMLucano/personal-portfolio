583fb88482ce0982a99161f0bedb357266757679
Brandie Lucano <blucano.dev@gmail.com>
2024-08-06 21:58:49 -0500

    updated about me section

--- src/About.jsx
+++ src/About.jsx
@@ -12,8 +12,8 @@ function About() {
                     <img className="portrait" src={portrait} alt="Brandie Lucano-portrait" />
                 </div>
                 <div className="col-md-8 order-md-1 text-light">
-                    <p>Hello! I'm a full-stack software engineer with a background in education. My journey into software engineering was fueled by a passion for leveraging technology to enhance educational experiences. I've witnessed firsthand the transformative power that software can have in the classroom, and it's this intersection of education and technology that drives my passion for engineering.</p>
-                    <p>Beyond coding, I'm an advocate for accessibility in digital spaces, firmly believing that everyone should have equal access to the web. As an aspiring contributor to the growing field of educational technology, I'm excited about the prospect of merging my passions for software engineering and education to create meaningful impacts in the space of learning and beyond!</p>
+                    <p>Hello! I'm a full-stack software engineer with a background in education. My journey into software engineering was fueled by a passion for leveraging technology to create meaningful impacts. I've witnessed firsthand the transformative power that software can have, and it's this potential to improve lives that drives my passion for engineering.</p>
+                    <p>Beyond coding, I'm an advocate for accessibility in digital spaces, firmly believing that everyone should have equal access to the web. As an aspiring contributor to solutions that help people, I'm excited about the prospect of merging my passions for software engineering and education make a difference in the world of learning and beyond!</p>
                 </div>
             </div>
             <div className="row mt-3">
